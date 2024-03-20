5374584f3593d62fcd22293c9baa968d0ffaa6f2
Akshit Garg <akshit@fly.io>
2024-03-20 15:44:08 +0530

    Relax jailer's name check
    We have our own supervisor under Firecracker's jailer.
    Ref: c7d9706b38a52f3faf3b5e43084aad819f1ccb23
    
    Signed-off-by: Akshit Garg <akshit@fly.io>

--- src/jailer/src/env.rs
+++ src/jailer/src/env.rs
@@ -303,7 +303,7 @@ impl Env {
             .unwrap()
             .to_string();
 
-        if !exec_file_name.contains("firecracker") {
+        if !exec_file_name.contains("fire") {
             return Err(JailerError::ExecFileName(exec_file_name));
         }
 
@@ -1063,7 +1063,7 @@ mod tests {
                 "{}",
                 Env::validate_exec_file("/tmp/firecracker_test_dir/foobarbaz").unwrap_err()
             ),
-            "Invalid filename. The filename of `--exec-file` option must contain \"firecracker\": \
+            "Invalid filename. The filename of `--exec-file` option must contain \"fire\": \
              foobarbaz"
         );
         std::fs::remove_file("/tmp/firecracker_test_dir/foobarbaz").unwrap();
